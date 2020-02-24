e53d0c601ec0b1a9edfdc45c00643db812669176
aastha1398 <aasthayadav6211@gmail.com>
2020-02-24 17:30:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-y2ugtw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-y2ugtw)
---
+++ index.ts
@@ -0,0 +1,47 @@
+import { of } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import { interval } from 'rxjs';
+import { take, finalize } from 'rxjs/operators';
+//import { toPromise} from 'rxjs/add/operators'
+
+// const source = of(1, 2, 3, 4, 5);
+
+// // tap also accepts an object map to log next, error, and complete
+// //  perform actions or side-effects, such as logging
+// const example = source.pipe(
+//  map(val => val + 10),
+//  tap({
+//     next: (val) => {
+//       // on next 11, etc.
+//       console.log('on next', val);
+//     },
+//     error: (error) => {
+//       console.log('on error', error.message);
+//     },
+//     complete: () => console.log('on complete')
+//   })
+// )
+// // output: 11, 12, 13, 14, 15
+// .subscribe(val => console.log(val));
+
+
+// //Call a function when observable completes or errors
+
+// //emit value in sequence every 1 second
+// const source = interval(1000);
+// //output: 0,1,2,3,4,5....
+// const example = source.pipe(
+//   take(5), //take only the first 5 values
+//   finalize(() => console.log('Sequence complete')) // Execute when the observable completes
+// )
+// const subscribe = example.subscribe(val => console.log(val));
+
+
+
+
+// // toPromise operator converts observable into promise
+// //return basic observable
+// const sample = val => of(val).pipe(delay(5000));
+// //convert basic observable to promise
+// const examplePromise = sample('First Example').pipe(toPromise())  //output: 'First Example'
+// .then(result => {    console.log('From Promise:', result);  });
